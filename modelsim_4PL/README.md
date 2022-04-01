10953134fbf46790e0058adf215e6fa19e0ebcc8
Ben David <amichai.ben.david@intel.com>
2022-04-01 18:44:49 +0300

    4 stage pipeline RTL - 6.13 performence with clk mul 2

---
+++ modelsim_4PL/README.md
@@ -0,0 +1,4 @@
+====The Commands=====  
+vlog.exe -f cpu_garage_list.f  
+vsim.exe work.cpu_garage_tb -c -do 'run -all'  
+vsim.exe -gui work.cpu_garage_tb  
---
+++ modelsim_4PL/cpu_garage_list.f
@@ -0,0 +1,13 @@
+// Includes
++incdir+../source/
+
+//Source File (PKG, RTL, MACROS)
+../source/alu.sv
+../source_4PL/cpu.sv
+../source/definitions.sv
+../source/ram.sv
+../source/rom.sv
+../source/cpu_garage.sv
+
+//Test Bench
+../source/cpu_garage_tb.sv
